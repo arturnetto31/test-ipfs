5ad5bb96c992c28ec4647449435036f6d476da46
Artur Neto <francisco.artur@dce.ufpb.br>
2022-09-18 13:21:41 -0300

    config /dist/

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: 'src',
-  base: '/dist/',
+  base: '',
   build: {
     outDir: '../dist',
   },
